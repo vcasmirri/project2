080401120709b6f0befbef636247ca34acc9eede
Cassandra Heil <Cassandraheil19@gmail.com>
2020-04-04 11:44:51 -0400

    got routes working with tables

---
+++ models/timeline.js
@@ -0,0 +1,10 @@
+module.exports = function(sequelize, DataTypes) {
+  var Todo = sequelize.define("Todo", {
+    text: DataTypes.STRING,
+    checked: DataTypes.BOOLEAN,
+    user_id: DataTypes.INTEGER,
+    dateChecked: DataTypes.DATEONLY,
+    // FOREIGN KEY (user_id) REFERENCES users(id)
+  });
+  return Todo;
+};
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,22 +3,22 @@ var db = require("../models");
 module.exports = function(app) {
   // Get all examples
   app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+    db.Todo.findAll({}).then(function(dbtodos) {
+      res.json(dbtodos);
     });
   });
 
   // Create a new example
   app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+    db.Todo.create(req.body).then(function(dbtodos) {
+      res.json(dbtodos);
     });
   });
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+    db.Todo.destroy({ where: { id: req.params.id } }).then(function(dbtodos) {
+      res.json(dbtodos);
     });
   });
 };
